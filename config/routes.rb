374b2c8cdccc359c6fc5320ff20221719ef28d8b
jonathanx111 <jonathanx111@gmail.com>
2021-01-27 23:32:53 -0500

    updated ruby version to 2.7.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :days
   resources :users
   post "/register", to: "auth#register"
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   patch "/tasks/:id", to: "tasks#edit_task"
   delete "/tasks/:id", to: "tasks#destroy"
   post "/tasks/", to: "tasks#create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
