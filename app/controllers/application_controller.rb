3b310d2a67e46e3579ae03719f77c23467c7f86e
jonathanx111 <jonathanx111@gmail.com>
2021-01-28 01:23:25 -0500

    changed back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
         # Fake auth
         # @user = User.first
 
-        unless !@user
+        unless @user
           render json: { error: "Unauthorized request" }, status: :unauthorized
         end
     # continue to the rest of the action
