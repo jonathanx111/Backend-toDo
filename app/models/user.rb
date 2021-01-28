a36a8f4b62f89a68dae2927c35922c013d157110
jonathanx111 <jonathanx111@gmail.com>
2021-01-28 18:13:09 -0500

    took out byebugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,20 @@ class User < ApplicationRecord
     validates :username, uniqueness: {case_sensitive: false}
 
     def self.from_google_signin(payload)
-        byebug
+        
         user = nil
         # find or create a user based on the email address from the Google payload
         User.where(username: payload["email"]).first_or_create do |new_user|
-            byebug
+            
             new_user.username = payload["email"]
             new_user.points = 0
             # we need to assign a password to satisfy bcrypt, so generate a random one...
-            byebug
+            
             new_user.password = SecureRandom.base64(15)
             user = new_user
-            byebug
+            
         end
-        byebug
+        
         return user
     end
 end
